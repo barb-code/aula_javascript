358d1324c920b61b23ee18aef04a32c5ebd1fe1e
Bárbara Lícia <74989625+barb-code@users.noreply.github.com>
2020-12-15 22:32:53 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,129 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>obrigado por clicar</b>";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar")
+}
+/*
+function redirecionar(){
+    window.open("https://github.com/barb-code");
+    //window.location.href = "https://github.com/barb-code";
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse"
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto")
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "Passe o mouse aqui"
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("Página carregada")
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+*/
+
+/*
+function soma(n1,n2){
+    return n1 + n2;
+}
+
+var validar = 0;
+
+function validaIdade(idade){
+    if(idade >=18){
+        validar = true
+
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?")
+validaIdade(idade)
+console.log(validar);
+*/
+/*
+var list = ["w","q","j"];
+list.push("x");
+print(list);
+/*
+/*
+var d = new Date();
+alert(d.getMonth()+1);
+alert(d.getHours());
+alert(d.getMilliseconds());
+*/
+
+
+
+//var count;
+//for(count=0; count <=5; count++){
+//    alert(count);
+//}
+
+/*
+var count = 0;
+while(count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+*/
+
+
+
+
+/*
+var idade = prompt("Qual a sua idade?");
+if (idade >=18){
+    alert("maior de idade")
+}else{
+    alert("menor de idade")
+};
+*/
+
+
+
+
+/*
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+
+
+/*
+var fruta = {nome:"maçã", cor:"vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+//var lista = ["maça","pêra","laranja"];
+//alert(lista[1]);
+//lista.push("uva")
+//lista.pop();
+
+//console.log(lista.toString()[0]);
+//console.log(lista[0]);
+//console.log(lista.join(" - "));
+
+
+//var nome = "Bárbara Lícia";
+//var idade = 24;
+//var idade2 = 13;
+//var frase = "India é o melhor time do mundo";
+//alert(nome + " tem " + idade)
+//alert(idade*idade2)
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace("India","Brasil"))
+//console.log(frase.toLocaleUpperCase());
